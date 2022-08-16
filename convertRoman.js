ffb0b18b01ba74fcdbd47da227937b12dcd79814
carloshbinda <carloshbinda@gmail.com>
2022-08-15 22:56:48 -0300

    code added to the repo for the first time

---
+++ convertRoman.js
@@ -0,0 +1,38 @@
+//The romanChar function receives a roman character and returns the
+//respective integer.
+function romanChar(roman) {
+  if (roman === "I") {
+    return 1;
+  } else if (roman === "V") {
+    return 5;
+  } else if (roman === "X") {
+    return 10;
+  } else if (roman === "L") {
+    return 50;
+  } else if (roman === "C") {
+    return 100;
+  } else if (roman === "D") {
+    return 500;
+  } else if (roman === "M") {
+    return 1000;
+  } else {
+    return 0;
+  }
+}
+
+//The function convertRoman converts a given roman number to the respective integer
+function convertRoman(roman) {
+  let sum = romanChar(roman[0]);
+  let temp = 0;
+  for (i = 0; i <= roman.length - 1; i++) {
+    if (romanChar(roman[i]) >= romanChar(roman[i + 1])) {
+      sum = sum + romanChar(roman[i + 1]);
+    } else {
+      sum = sum - romanChar(roman[i]);
+      temp = romanChar(roman[i + 1]) - romanChar(roman[i]);
+      sum = sum + temp;
+    }
+  }
+  return sum;
+}
+console.log(convertRoman("IX"));
